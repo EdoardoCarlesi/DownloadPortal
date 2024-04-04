6825ff9aaaea210d86bbb5daebb6f694595c28a6
Edoardo ChromeOS <ecarlesi83@gmail.com>
2024-04-04 20:54:31 +0200

    login and register update

--- xxyears/auth.py
+++ xxyears/auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint, flash, g, redirect, render_template, request, sessi
 from werkzeug.security import check_password_hash, generate_password_hash
 try:
     from xxyears.db import get_db
-    import xxyears.codes
+    import xxyears.codes as codes
 except:
     from db import get_db
     import codes
@@ -15,14 +15,18 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+
+    print(codes.draw_random_code())
+
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         code = request.form['code']
         db = get_db()
         error = None
-        if not username:
-            error = 'Username is required.'
+
+        if not email:
+            error = 'Email is required.'
         elif not password:
             error = 'Password is required.'
         elif not code:
@@ -36,13 +40,14 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password, code) VALUES (?, ?, ?)",
-                    (username, generate_password_hash(password), code),
+                    "INSERT INTO user (email, password, code) VALUES (?, ?, ?)",
+                    (email, generate_password_hash(password), code),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"User {username} or code {code} is already registered. Try registering with a different code or username."
+                error = f"Code {code} or email {email} is already registered. Try registering with a different code or email."
             else:
+                flash('Registration successful. You can now login.')
                 return redirect(url_for("auth.login"))
 
         flash(error)
@@ -55,23 +60,24 @@ def login():
     session['logged_in'] = False
 
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+        mail = db.execute(
+            'SELECT * FROM user WHERE email = ?', (email,)
         ).fetchone()
 
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        if mail is None:
+            error = 'Incorrect email.'
+        elif not check_password_hash(mail['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session['user_id'] = email
             session['logged_in'] = True
+            flash('You logged in successfully')
             return redirect(url_for('video.play'))
 
         flash(error)
